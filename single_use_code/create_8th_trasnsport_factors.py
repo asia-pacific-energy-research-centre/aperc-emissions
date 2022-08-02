b77b610267946a99ef91d42339fce5d4b1ed688b
Finbar Barton Maunsell <finn.maunsell@gmail.com>
2022-08-02 21:29:17 +0900

    a few fixes

---
+++ single_use_code/create_8th_trasnsport_factors.py
@@ -0,0 +1,186 @@
+#%%
+#this file is intended to calcualte emission factors for the 8th edition of the transport data as there are no emission factors for some 'general' fuel types:  ['1_x_coal_thermal', '16_x_hydrogen', '7_x_jet_fuel']. Although, in doing so, it was realised that there is very little effect of this long process, and probably wouldv been better to save the effort. oh well.
+
+execfile("../config/config.py")#usae this to load libraries and set variables. Feel free to edit that file as you need
+
+#%%
+#load mapping for transport 8th (created manually)
+transport_8th_mapping = pd.read_excel('{}/emissions_to_energy_mappings.xlsx'.format(config_folder_path), sheet_name ='transport_8th')
+
+#laod energy datas from aperc_transport
+energy = pd.read_csv('../../aperc_transport/intermediate_data/energy.csv'.format(intermediate_data_path))
+
+apec_mapping = pd.read_excel('{}/emissions_to_energy_mappings.xlsx'.format(config_folder_path), sheet_name ='00APEC_map')
+
+generation_emissions_dtf = pd.read_csv('{}/egeda_generation_emissions_factors.csv'.format(output_data_path))
+
+emissions_factors = pd.read_csv('{}/{}'.format(output_data_path, '00APEC_emissions_factors.csv'))
+
+transport_energy = pd.read_csv('../../aperc_transport/output_data/00APEC_transport.csv')
+
+#%%
+#first get transport emisison factors from full set of emission factors 
+
+#to retrieve from emission factrors in the output apec datafra,me we need similar fuel codes. So we map to the apec fuel code mapping using transport 8th mapping then apec mapping to eventually get the emissions factrrors with the same fuel codes as are used in transport 8th
+transport_8th_mapping = transport_8th_mapping.merge(apec_mapping, on = 'IPCC_emission_factors_2021_fuel', how = 'left')
+
+transport_8th_mapping.rename(columns={'00_APEC_fuel_code': 'fuel_code'}, inplace=True)
+
+#drop cols just to make easy
+transport_8th_mapping_fuels = transport_8th_mapping[['fuel_code', 'Fuel_transport_8th']].drop_duplicates()
+
+#filter for only the fuel types in factors df we want using left join
+transport_emission_factors = transport_8th_mapping_fuels.merge(emissions_factors, how='left', on='fuel_code')
+
+#remove the fuel code column
+transport_emission_factors = transport_emission_factors.drop(columns=['fuel_code']).drop_duplicates()
+
+#now we have the emission factors for the transport fuel types in 8th
+#%%
+#  calc avg of last X year of generation factors for aeach economy
+
+X = 5#avg emission factors of latest X years.
+latest_year = max(generation_emissions_dtf['Year'])
+
+generation_emissions_dtf2 =generation_emissions_dtf.copy()
+
+#filter for only latest X years
+generation_emissions_dtf2 = generation_emissions_dtf2[generation_emissions_dtf2["Year"].isin(range(latest_year-X,latest_year))]
+#calc
+generation_emissions_dtf2_mean = generation_emissions_dtf2.groupby(['Economy']).mean().reset_index()
+
+#grab economy and emissions factor only
+generation_emissions_dtf2_mean = generation_emissions_dtf2_mean[['Economy', 'Emissions factor (MT/PJ)']]
+#%%
+#make orignal data have every economy
+transport_emission_factors_all_econ = transport_emission_factors.copy()
+
+generation_emissions_dtf2_mean_only_econ = generation_emissions_dtf2_mean[['Economy']].drop_duplicates()
+
+transport_emission_factors_all_econ = transport_emission_factors_all_econ.merge(generation_emissions_dtf2_mean_only_econ, how='cross')
+
+#now replace elec data in roginal with elec data from generation_emissions_dtf2_mean
+generation_emissions_dtf2_mean['Fuel_transport_8th'] = '17_electricity'
+transport_emission_factors_all_econ = transport_emission_factors_all_econ[transport_emission_factors_all_econ['Fuel_transport_8th'] != '17_electricity']
+transport_emission_factors_all_econ = pd.concat([transport_emission_factors_all_econ, generation_emissions_dtf2_mean])
+
+#%%
+FUEL_TYPES_IN_QUESTION = ['1_x_coal_thermal', '16_x_hydrogen', '7_x_jet_fuel']
+#Now we wnat to replacce the values that dont match the mappings with avgs of the last X years
+
+#we are going to have to do this by finding the proportion of total for each fuel type that could be in these general fuel types (eg. coal_thermal) then times those proprtions by the emission factor for those fuel types, then sum them
+
+#will specify the fuel ttypes in question manully to reduce complexity. We will concat these two dtfs later.
+avg_fuel_types = transport_emission_factors_all_econ[transport_emission_factors_all_econ['Fuel_transport_8th'].isin(FUEL_TYPES_IN_QUESTION)]
+not_avg_fuel_types = transport_emission_factors_all_econ[~transport_emission_factors_all_econ['Fuel_transport_8th'].isin(FUEL_TYPES_IN_QUESTION)]
+
+#hydrogen is assumed to be 0
+avg_fuel_types.loc[(avg_fuel_types['Fuel_transport_8th'] == '16_x_hydrogen'), 'Emissions factor (MT/PJ)'] = 0
+
+#make hisotric transport energy long format and year as an int in preparation for next steps using this data
+transport_energy_long = transport_energy.melt(id_vars=['Fuel', 'Sector', 'Economy'], var_name = 'Year', value_name = 'Energy')
+transport_energy_long['Year'] = transport_energy_long['Year'].astype(int)
+
+
+#%%
+#first lets do jet fuel:
+
+#for each of these fuel types we will find the similar fuel types in transport energy use then sum them for the last X years, and find sum of each individual fuel type as a proprtion of total
+x_jet_fuel = transport_energy_long[transport_energy_long['Fuel'].isin(['7.02 Aviation gasoline', '7.04 Gasoline type jet fuel', '7.05 Kerosene type jet fuel'])]
+
+#filter for only latest X years
+x_jet_fuel = x_jet_fuel[x_jet_fuel["Year"].isin(range(latest_year-X,latest_year))]
+
+#calc proportion
+x_jet_fuel_sum = x_jet_fuel.groupby(['Economy', 'Fuel']).sum().reset_index()
+
+x_jet_fuel_sum = x_jet_fuel_sum[['Economy', 'Fuel', 'Energy']]
+
+x_jet_fuel_sum2 = x_jet_fuel_sum.groupby(['Economy']).sum().reset_index()
+
+x_jet_fuel_sum2 = x_jet_fuel_sum2[['Economy', 'Energy']]
+
+x_jet_fuel_sum_merge = x_jet_fuel_sum.merge(x_jet_fuel_sum2, how='left', on='Economy')
+
+x_jet_fuel_prop= x_jet_fuel_sum_merge.copy()
+x_jet_fuel_prop['Proportion'] = x_jet_fuel_prop['Energy_x']/x_jet_fuel_prop['Energy_y']
+
+#now we have the proportion of each fuel type in the total for each economy we join that onto the emission factrors
+x_jet_fuel_prop = x_jet_fuel_prop[['Economy', 'Fuel', 'Proportion']]
+x_jet_fuel_prop.rename(columns={'Fuel': 'fuel_code'}, inplace=True)
+x_jet_fuel_prop_merge = x_jet_fuel_prop.merge(emissions_factors, how='left', on='fuel_code')
+
+x_jet_fuel_prop_merge['Emissions factor (MT/PJ)'] = x_jet_fuel_prop_merge['Proportion']*x_jet_fuel_prop_merge['Emissions factor (MT/PJ)']
+
+x_jet_fuel_sum_ef = x_jet_fuel_prop_merge.groupby(['Economy']).sum().reset_index()
+
+#give it the same fuel code as is used in 8th energy data  and remove unneccsary cols so we can join it onto central df
+x_jet_fuel_sum_ef['Fuel_transport_8th'] = '7_x_jet_fuel'
+x_jet_fuel_sum_ef = x_jet_fuel_sum_ef[['Economy', 'Fuel_transport_8th', 'Emissions factor (MT/PJ)']]
+
+#we have an issue where some economys will now have emission factors of 0 for these fuel types because they dont use that fuel, yet. In this case we will replcae them, with the avg of all other, non-0, emission factors for that fuel type
+non_zero_jet_fuel = x_jet_fuel_sum_ef[x_jet_fuel_sum_ef['Emissions factor (MT/PJ)']>0]
+non_zero_jet_fuel_avg = np.mean(non_zero_jet_fuel['Emissions factor (MT/PJ)'])
+
+x_jet_fuel_sum_ef['Emissions factor (MT/PJ)'].replace(0, non_zero_jet_fuel_avg, inplace=True)
+
+#remove jet fuel from avg_fuel_types then concat new data
+avg_fuel_types = avg_fuel_types[~avg_fuel_types['Fuel_transport_8th'].isin(['7_x_jet_fuel'])]
+
+avg_fuel_types = pd.concat([avg_fuel_types, x_jet_fuel_sum_ef])
+#%%
+#now for coal thermal using same strategy:
+
+#for each of these fuel types we will find the similar fuel types in transport energy use then sum them for the last X years, and find sum of each individual fuel type as a proprtion of total
+x_coal_thermal = transport_energy_long[transport_energy_long['Fuel'].isin(['1.2 Other bituminous coal', '1.3 Sub-bituminous coal', '1.4 Anthracite', '1.5 Lignite'])]
+
+#filter for only latest X years
+x_coal_thermal = x_coal_thermal[x_coal_thermal["Year"].isin(range(latest_year-X,latest_year))]
+
+#calc proportion
+x_coal_thermal_sum = x_coal_thermal.groupby(['Economy', 'Fuel']).sum().reset_index()
+
+x_coal_thermal_sum = x_coal_thermal_sum[['Economy', 'Fuel', 'Energy']]
+
+x_coal_thermal_sum2 = x_coal_thermal_sum.groupby(['Economy']).sum().reset_index()
+
+x_coal_thermal_sum2 = x_coal_thermal_sum2[['Economy', 'Energy']]
+
+x_coal_thermal_sum_merge = x_coal_thermal_sum.merge(x_coal_thermal_sum2, how='left', on='Economy')
+
+x_coal_thermal_prop= x_coal_thermal_sum_merge.copy()
+x_coal_thermal_prop['Proportion'] = x_coal_thermal_prop['Energy_x']/x_coal_thermal_prop['Energy_y']
+
+#now we have the proportion of each fuel type in the total for each economy we join that onto the emission factrors
+x_coal_thermal_prop = x_coal_thermal_prop[['Economy', 'Fuel', 'Proportion']]
+x_coal_thermal_prop.rename(columns={'Fuel': 'fuel_code'}, inplace=True)
+x_coal_thermal_prop_merge = x_coal_thermal_prop.merge(emissions_factors, how='left', on='fuel_code')
+
+x_coal_thermal_prop_merge['Emissions factor (MT/PJ)'] = x_coal_thermal_prop_merge['Proportion']*x_coal_thermal_prop_merge['Emissions factor (MT/PJ)']
+
+x_coal_thermal_sum_ef = x_coal_thermal_prop_merge.groupby(['Economy']).sum().reset_index()
+
+#give it the same fuel code as is used in 8th energy data  and remove unneccsary cols so we can join it onto central df
+x_coal_thermal_sum_ef['Fuel_transport_8th'] = '1_x_coal_thermal'
+x_coal_thermal_sum_ef = x_coal_thermal_sum_ef[['Economy', 'Fuel_transport_8th', 'Emissions factor (MT/PJ)']]
+
+#we have an issue where some economys will now have emission factors of 0 for these fuel types because they dont use that fuel, yet. In this case we will replcae them, with the avg of all other, non-0, emission factors for that fuel type
+non_zero_coal_thermal = x_coal_thermal_sum_ef[x_coal_thermal_sum_ef['Emissions factor (MT/PJ)']>0]
+non_zero_coal_thermal_avg = np.mean(non_zero_coal_thermal['Emissions factor (MT/PJ)'])
+
+x_coal_thermal_sum_ef['Emissions factor (MT/PJ)'].replace(0, non_zero_coal_thermal_avg, inplace=True)
+
+#remove jet fuel from avg_fuel_types then concat new data
+avg_fuel_types = avg_fuel_types[~avg_fuel_types['Fuel_transport_8th'].isin(['1_x_coal_thermal'])]
+
+avg_fuel_types = pd.concat([avg_fuel_types, x_coal_thermal_sum_ef])
+# %%
+#now finally join all the fuel types together
+emission_factors_for_8th_edition_transport = pd.concat([avg_fuel_types, not_avg_fuel_types])
+
+
+#%%
+#save and be done wiht this mess
+emission_factors_for_8th_edition_transport.to_csv('../output_data/emission_factors_for_8th_edition_transport.csv', index=False)
+
+#%%
---
+++ workflow/2_calc_elec_gen_emission_factors_for_egeda_data.py
@@ -0,0 +1,113 @@
+#intention is to create a system to calc emissions for elec generation. 
+#However it is difficult to create a one size fits all system since the input energy data is going to have different categories. However we can creeate an easy to follow example for the user to replciate for their needs
+
+#This example will take in the energy used to generate the electricity, as well as the total electricity geenrated. it will sum up the energy used to generate electricity and the total electricity generated and then calculate the emissions using emisisons_of_elec_gen / PJ_of_elec_gen.
+#this example will also be done on the whole of the 00APEC file so it will calc gen emissions for historical data for all economys
+#%%
+
+execfile("../config/config.py")#usae this to load libraries and set variables. Feel free to edit that file as you need
+
+#%%
+
+#load electricity generation data
+#note that this needs to be put in a format that is compatable with the emissions factors file. Basically a fuel_code column and a PJ column. #eg this is the origianl format of each sheet of the xlsx file we will load in
+#Economy, Year, Fuel, PJ
+# eg. 01_Aus, 2010, 17_electricity, 0.0  #this is the electricity output data
+# eg. 01_Aus, 2011, 7_8_fuel_oil, 0.0  #this is the fuel oil input data
+
+energy_data_file = '../input_data/00APEC_FUELSUMSREMOVED.xlsx'#CHANGE ME. 
+emissions_factors_file = '00APEC_emissions_factors.csv'#CHANGE ME.
+#%%
+#LOAD
+emissions_factors = pd.read_csv('{}/{}'.format(output_data_path, emissions_factors_file))
+# energy_data = pd.read_excel('{}/{}'.format(input_data_path, energy_data_file))
+
+#%%
+#FORMAT
+#rename fuel_code to Fuel
+emissions_factors = emissions_factors.rename(columns={'fuel_code':'Fuel'})
+
+#create example using eachh sheet of the 00_APEC data
+generation_emissions_dtf = pd.DataFrame(columns=['Economy', 'Year','Emissions factor (MT/PJ)'])#create empty dtf. We will add all newly created emissions factors to it
+for sheet in pd.ExcelFile(energy_data_file).sheet_names:
+    APEC_data = pd.read_excel(energy_data_file, sheet_name=sheet)
+    
+    #make data tall 
+    APEC_data = APEC_data.melt(id_vars=['Fuel', 'Sector'], var_name='Year', value_name='PJ')
+    #remove years that are not integers, as they are missing data
+    APEC_data = APEC_data[APEC_data['Year'].apply(lambda x: isinstance(x, int))]
+
+    #filter for only the sectors that we want () #note that this excludes energy used in CHP to produce electricity and heat by autoproducers and main activity roducers. This is because we have no way to split the heat production from the electricity generation for CHP, and it seems more likely that the heat prdction will make up a majority of CHP input energy.
+    APEC_input_energy_data = APEC_data[APEC_data['Sector'].isin(['9.1.1 Electricity plants', '9.2.1 Electricity plants'])]
+    APEC_input_energy_data = APEC_input_energy_data[APEC_input_energy_data['Fuel']!='17 Electricity']#emissions will be set to 0 for this anyway, but best to be sure
+
+    APEC_output_energy_data = APEC_data[APEC_data['Sector'].isin(['9. Total transformation sector'])]
+    APEC_output_energy_data = APEC_output_energy_data[APEC_output_energy_data['Fuel'].isin(['17 Electricity'])]
+
+    #sum data by Fuel type and year
+    APEC_input_energy_data = APEC_input_energy_data.groupby(['Fuel', 'Year']).sum().reset_index()
+    APEC_output_energy_data = APEC_output_energy_data.groupby(['Fuel', 'Year']).sum().reset_index()
+
+    #convert input data to positives
+    APEC_input_energy_data['PJ'] = APEC_input_energy_data['PJ'].abs()
+
+    
+    #Check if there are any fuel codes are in the energy data set that arent in the emissions factors set:
+    list_2 = emissions_factors['Fuel'].unique()
+    list_1 = APEC_input_energy_data['Fuel'].unique()
+    missing_codes_list = [x for set_1 in (set(list_2),) for x in list_1 if x not in set_1]
+    if len(missing_codes_list)>0:
+        print("These fuel codes are in the energy data set for sheet {} but arent in the emissions factors set. You will need to do somethig about them: ".format(sheet), missing_codes_list)
+
+    
+    #merge input data with emission factors  
+    APEC_input_energy_data = pd.merge(APEC_input_energy_data, emissions_factors, on='Fuel', how='left')
+
+    #and then times them,
+    APEC_input_energy_data['Emissions'] = APEC_input_energy_data['PJ'] * APEC_input_energy_data['Emissions factor (MT/PJ)']
+
+    #sum the emissions per year
+    APEC_input_energy_data_emissions_sum = APEC_input_energy_data[['Year', 'Emissions']].groupby(['Year']).sum().reset_index()
+
+    #merge the emissions per year with the output electricity data
+    APEC_output_energy_data = pd.merge(APEC_output_energy_data, APEC_input_energy_data_emissions_sum, on='Year', how='left')
+
+    #divide the emissions by the pj to get emisisons factor MT/PJ of emissions for elec produciton in the year
+    APEC_output_energy_data['Emissions factor (MT/PJ)'] = APEC_output_energy_data['Emissions'] / APEC_output_energy_data['PJ']
+
+    #lastly make a column which specs what economy the generation data is from so that wen we look at the dtf of all emisison factors we can se what econmomy it is for
+    APEC_output_energy_data['Economy'] = sheet
+    APEC_output_energy_data['Fuel'] = '17 Electricity'
+    APEC_output_energy_data = APEC_output_energy_data[['Economy', 'Year', 'Fuel', 'Emissions factor (MT/PJ)']]
+
+    generation_emissions_dtf = pd.concat([generation_emissions_dtf, APEC_output_energy_data], axis=0)
+
+#%%
+#Check if there are any fuel codes are in the energy data set that arent in the emissions factors set:
+list_2 = emissions_factors['Fuel'].unique()
+list_1 = APEC_input_energy_data['Fuel'].unique()
+missing_codes_list = [x for set_1 in (set(list_2),) for x in list_1 if x not in set_1]
+if len(missing_codes_list)>0:
+    print("These fuel codes are in the energy data set for sheet {} but arent in the emissions factors set. You will need to do somethig about them: ".format(sheet), missing_codes_list)
+
+
+#%%
+#join emissions factors to the newly calculated generation data
+#load
+#create a copy of the emissions factors foir each economy and year in gen_factors (which is every economy year combination in the 00_APEC dataset) then concat the two dataframes. this will make it so we ahve a set of emission factors for each eocnomy and year in the apec data so we can calculate the emissions for each economy easily, given that the emissions factors for elec use is different wfor eadch economy.
+dummy_df = generation_emissions_dtf[['Economy', 'Year']].drop_duplicates()
+
+apec_factors_2 = dummy_df.merge(emissions_factors, how='cross')#cross joins are amazing
+
+#concatenate gen and emissionfactors
+large_emissions_factors_df = pd.concat([generation_emissions_dtf, apec_factors_2], axis=0)
+
+#%%
+#SAVE
+generation_emissions_dtf.to_csv('{}/egeda_generation_emissions_factors.csv'.format(output_data_path), index=False)
+
+large_emissions_factors_df.to_csv('{}/egeda_all_emissions_factors.csv'.format(output_data_path), index=False)
+
+#%%
+#wish list, example using 8th edition data as inpt *would require using finns transport scripts that take in data from 8th edition charts outputs, change it to take in data from sheets for 'Power Fuel Consumption' and then format. 
+# %%
